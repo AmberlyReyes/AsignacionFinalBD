c49fa7a8ad4e53c8dee5301a24ef79b85525840b
PC <PC@LAPTOP-036AI9G5>
2024-07-25 15:52:23 -0400

    Arreglo de la conexion

--- AsignacionFinal/src/Conexion/Conexion.java
+++ AsignacionFinal/src/Conexion/Conexion.java
@@ -2,8 +2,10 @@ package Conexion;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Driver;
 
 
+/*
 public class Conexion {
 	
 	
@@ -22,7 +24,7 @@ public class Conexion {
 			
 		}
 	}*/
-	Connection conectar=null;
+	/*Connection conectar=null;
 	
 	String usuario="MGMZ";
 	String contrasenna="1234";
@@ -36,7 +38,7 @@ public class Conexion {
 		
 		try {
 			String cadena="jdbc:sqlserver://localhost"+ puerto+";"+"dataBaseName="+bd;
-			conectar= DriverManager.getConnection(cadena,usuario,contrasenna);
+			conectar= DriverManager.getConnection(cadena, usuario, contrasenna);
 			
 			
 		}catch (Exception e) {
@@ -46,6 +48,47 @@ public class Conexion {
 		}
 		return conectar;
 		
+	}*/
+	
+	/*public static void main(String[] args) {
+		// TODO code application logic here
+		
+		String conexion= "jdbc:sqlserver://localhost:1443;" + "databaseName=Academica";
+		Connection conector = null;
+		
+		try 
+		{
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			conector= DriverManager.getConnection (conexion, "MGMZ", "1234"); 
+			System.out.println("conexion exitosa ");
+		}
+		catch (Exception e) {
+			System.out.println("error de conexion ");
+		}
 	}
 
+}
+*/
+
+
+public class Conexion {
+	private static Connection cn;
+	public static Connection getConnection(){
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			cn=DriverManager.getConnection("jdbc:sqlserver://localhost:1433; databasename= Academica", "MGMZ","1234");
+		} catch (Exception e) {
+			cn=null;
+		}
+			return cn;
+		}
+		public static void main(String[] args){
+		Connection pruebaCn= Conexion.getConnection();
+		if(pruebaCn!=null){
+			System.out.println("Conectado"); 
+			System.out.println(pruebaCn);
+		}else{
+			System.out.println("Desconectado");
+		}
+	}
 }
